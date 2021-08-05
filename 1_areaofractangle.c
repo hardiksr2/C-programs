5d3d10258d5601893d6a4608593211f53e885bae
Hardik Rathod <75353940+hardiksr2@users.noreply.github.com>
2021-08-05 13:08:39 +0530

    Add files via upload

---
+++ 1_areaofractangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int width = 10, length = 15;
+    int area = width * length;
+    printf("Area of ractangle is %d", area);
+    return 0;
+}
---
+++ 1_areaofractangleb.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int width, length;
+    printf("Width of the ractangle \n");
+    scanf("%d", &width);
+    printf("length of the ractangle \n");
+    scanf("%d", &length);
+    printf("Area of your ractange is: %d", width * length);
+    return 0;
+}

743d20929f40cfd60c78f10eb79364059dd2f2bc
Hardik Rathod <75353940+hardiksr2@users.noreply.github.com>
2021-08-05 13:50:18 +0530

    Add files via upload

---
+++ celciustofarenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    float celcius = 37, far;
+    far = (celcius * 9/5) + 32;
+    printf("The value of this celcius temprature in farenheit is =%f", far);
+    return 0;
+}
+//human body temprature is the value of farenhiet

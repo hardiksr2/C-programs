cdda6de9ad31f2de7cd968fa56f0abf94fef7ead
Hardik Rathod <75353940+hardiksr2@users.noreply.github.com>
2021-08-05 12:38:39 +0530

    Add files via upload

---
+++ 1_first.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World, I am learning C language.");
+    return 0;
+}
---
+++ 2_adding2num.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a = 5;
+    int b = 10;
+
+    printf("this two number in c: %d %d", a, b);
+    return 0;
+}
